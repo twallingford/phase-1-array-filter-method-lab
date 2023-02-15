2db47452214b2205a7922359b6ae4c22ff049b3a
Thomas Wallingford <tbwallingford@gmail.com>
2023-02-14 17:29:40 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution here
+const anArray = ['Hello', 'Hi', 'Howdy', 'Ahoy'];
+
+function findMatching(drivers, searchTerm){
+    return drivers.filter(function (driver) {
+        if(driver.toLowerCase() === searchTerm.toLowerCase()){
+            return driver
+        }
+    })
+}
+
+function fuzzyMatch(drivers, searchTerm){
+    const searchLength = searchTerm.length
+    return drivers.filter(function (driver) {
+        if(driver.slice(0, searchLength).toLowerCase() === searchTerm.toLowerCase()){
+            return driver 
+        }
+    })
+}
+
+
+function matchName(drivers, searchTerm){
+    return drivers.filter(function (driver){
+        if(driver.name === searchTerm){
+            return driver.name
+        }
+    })
+}
+
+// findMatching() takes an array of drivers' names and a string as arguments. Returns array of matching drivers. Case insensitive.
+// fuzzyMatch() takes an array of drivers' names and a string as arguments. Returns all drivers whose names begin with the provided letters.
+// matchName() takes an array of drivers' names and a string as arguments. Each driver has two properties (name, hometown). Function
+// returns each element whose name property matches the provided string argument
